3c7bd6f58038ef638c39789ce689bbf8a7a57968
Peter Bronkalla <bronkalla@sipgate.de>
2013-09-27 10:26:06 +0200

    Changed output to only the text and not the key too.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 function start(response) {
 	console.log("Request handler 'start' was called.");
@@ -16,16 +16,17 @@ function start(response) {
 		'</form>'+
 		'</body>'+
 		'</html>';
-	
+
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write(body);
-	response.end();	
+	response.end();
 }
 
 function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("You've sent: " + postData);
+		response.write("You've sent the text: " +
+		querystring.parse(postData).text);
 		response.end();
 }
 
